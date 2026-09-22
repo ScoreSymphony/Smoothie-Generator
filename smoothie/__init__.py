ba99b2f17f085e12797acac69849221b893ae2ab
Score Symphony <scoresymphony@gmail.com>
2026-09-22 17:35:59 +0200

    M3: export recipe matching API

--- smoothie/__init__.py
+++ smoothie/__init__.py
@@ -1,15 +1,9 @@
 """Core Smoothie Generator domain package."""
-
 from .ingredient_catalog import IngredientCatalog, load_ingredient_catalog
 from .ingredients import AmountRange, Ingredient, IngredientCategory
+from .matcher import RecipeMatch, match_recipe, rank_recipes
 from .pantry import PantryState, parse_free_text
+from .recipe_catalog import load_recipe_catalog
+from .recipes import Recipe, RecipeIngredient, SubstitutionGroup
 
-__all__ = [
-    "AmountRange",
-    "Ingredient",
-    "IngredientCatalog",
-    "IngredientCategory",
-    "PantryState",
-    "load_ingredient_catalog",
-    "parse_free_text",
-]
+__all__=["AmountRange","Ingredient","IngredientCatalog","IngredientCategory","PantryState","Recipe","RecipeIngredient","RecipeMatch","SubstitutionGroup","load_ingredient_catalog","load_recipe_catalog","match_recipe","parse_free_text","rank_recipes"]
