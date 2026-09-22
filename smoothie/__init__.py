4c014bac71db1e89e11b5c0c7c52652e92c94e11
Score Symphony <scoresymphony@gmail.com>
2026-09-22 17:30:23 +0200

    M2: export pantry domain model

--- smoothie/__init__.py
+++ smoothie/__init__.py
@@ -2,5 +2,6 @@
 
 from .ingredient_catalog import IngredientCatalog, load_ingredient_catalog
 from .ingredients import AmountRange, Ingredient, IngredientCategory
+from .pantry import Pantry
 
-__all__=["AmountRange","Ingredient","IngredientCatalog","IngredientCategory","load_ingredient_catalog"]
+__all__=["AmountRange","Ingredient","IngredientCatalog","IngredientCategory","Pantry","load_ingredient_catalog"]
