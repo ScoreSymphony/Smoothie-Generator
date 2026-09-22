11374ed3934dbc79a4eb1d01f650562ce477da70
Score Symphony <scoresymphony@gmail.com>
2026-09-22 18:18:18 +0200

    M6: export quantity and nutrition API

--- smoothie/__init__.py
+++ smoothie/__init__.py
@@ -3,7 +3,21 @@ from .generator import GeneratedSmoothie, SmoothieGenerator
 from .ingredient_catalog import IngredientCatalog, load_ingredient_catalog
 from .ingredients import AmountRange, Ingredient, IngredientCategory
 from .matcher import RecipeMatch, match_recipe, rank_recipes
+from .nutrition import (
+    NutritionCalculator,
+    NutritionCatalog,
+    NutritionFacts,
+    load_nutrition_catalog,
+)
 from .pantry import PantryState, parse_free_text
+from .quantities import (
+    QuantifiedIngredient,
+    QuantifiedRecipe,
+    QuantifiedSmoothie,
+    Quantity,
+    QuantityCalculator,
+    QuantityUnit,
+)
 from .recipe_catalog import load_recipe_catalog
 from .recipes import Recipe, RecipeIngredient, SubstitutionGroup
 from .scoring import (
@@ -22,7 +36,16 @@ __all__ = [
     "Ingredient",
     "IngredientCatalog",
     "IngredientCategory",
+    "NutritionCalculator",
+    "NutritionCatalog",
+    "NutritionFacts",
     "PantryState",
+    "QuantifiedIngredient",
+    "QuantifiedRecipe",
+    "QuantifiedSmoothie",
+    "Quantity",
+    "QuantityCalculator",
+    "QuantityUnit",
     "Recipe",
     "RecipeIngredient",
     "RecipeMatch",
@@ -31,6 +54,7 @@ __all__ = [
     "SmoothieGenerator",
     "SubstitutionGroup",
     "load_ingredient_catalog",
+    "load_nutrition_catalog",
     "load_recipe_catalog",
     "match_recipe",
     "parse_free_text",
