4e3af00a5ae2b816b43b24de44fbfb80af3087ac
Score Symphony <scoresymphony@gmail.com>
2026-09-22 18:09:38 +0200

    M4: add rule-based smoothie generation engine (#17)
    * M4: add rule-based smoothie generation engine
    
    * M4: test deterministic structured generation
    
    * M4: export smoothie generator API
    
    * M4: show generated smoothie candidates in UI
    
    * M4: fix package generator exports
    
    * M4: canonicalize pantry order for deterministic generation
    
    * M4: wire generated candidates into Streamlit UI
    
    * M4: restore recipe matches and render generated candidates
    
    * M4: avoid false incompatibility rejections
    
    * M4: cover valid sparse-tag combinations
    
    * Audit: cover generated and stored recommendation UI

--- smoothie/__init__.py
+++ smoothie/__init__.py
@@ -1,4 +1,5 @@
 """Core Smoothie Generator domain package."""
+from .generator import GeneratedSmoothie, SmoothieGenerator
 from .ingredient_catalog import IngredientCatalog, load_ingredient_catalog
 from .ingredients import AmountRange, Ingredient, IngredientCategory
 from .matcher import RecipeMatch, match_recipe, rank_recipes
@@ -6,4 +7,4 @@ from .pantry import PantryState, parse_free_text
 from .recipe_catalog import load_recipe_catalog
 from .recipes import Recipe, RecipeIngredient, SubstitutionGroup
 
-__all__=["AmountRange","Ingredient","IngredientCatalog","IngredientCategory","PantryState","Recipe","RecipeIngredient","RecipeMatch","SubstitutionGroup","load_ingredient_catalog","load_recipe_catalog","match_recipe","parse_free_text","rank_recipes"]
+__all__=["GeneratedSmoothie","SmoothieGenerator","AmountRange","Ingredient","IngredientCatalog","IngredientCategory","PantryState","Recipe","RecipeIngredient","RecipeMatch","SubstitutionGroup","load_ingredient_catalog","load_recipe_catalog","match_recipe","parse_free_text","rank_recipes"]
